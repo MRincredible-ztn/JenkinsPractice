41d5657f09ea6af82b48c4e1911f6971e9678d61
zhaotianneng <xianligongdaxue448@gmail.com>
2020-07-09 02:49:24 -0400

    Create project files

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+
+
+public class MyClass {
+/*
+ * this is my first program
+ */
+	public static void main(String[] args) {
+//		 TODO Auto-generated method stub
+//         this is my first comment
+		System.out.println("Hello guys");
+		
+
+	}
+
+}
